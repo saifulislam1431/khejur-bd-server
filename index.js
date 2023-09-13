6e4d3802aea7c0c1c279a63ae95a9ba168af1a7c
Saiful Islam <sakibbhaiya143@gmail.com>
2023-09-13 21:16:14 +0600

    User and cart api

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8cnv71c.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@khejurbd.2z99p4m.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "Unauthorized Access" })
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: "Unauthorized Access" })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
 
 
 
@@ -27,7 +42,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        const allProductCollection = client.db("khejurDB").collection("products");
+        const allProductCollection = client.db("KhejurDB").collection("products");
+        const cartCollection = client.db("KhejurDB").collection("cart");
+        const usersCollection = client.db("KhejurDB").collection("users");
+
+        // JWT
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "4h" })
+            res.send({ token })
+        })
 
 
 
@@ -37,6 +61,28 @@ async function run() {
         app.get('/all-products', async (req, res) => {
             const result = await allProductCollection.find({}).toArray();
             res.send(result)
+        })
+
+        // Cart Api
+        app.post("/product-cart", verifyJWT, async (req, res) => {
+            const newData = req.body;
+            const result = await cartCollection.insertOne(newData);
+            res.send(result);
+        })
+
+
+
+        // users api
+        app.post("/users", async (req, res) => {
+            const newUser = req.body;
+            const email = { email: newUser.email };
+            const existUser = await usersCollection.findOne(email);
+            if (existUser) {
+                return res.json("User Exist!")
+            } else {
+                const result = await usersCollection.insertOne(newUser);
+                return res.send(result);
+            }
         })
 
 
