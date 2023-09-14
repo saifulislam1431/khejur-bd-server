140a7c66f801a9578c1fb61be674857095d64e79
Saiful Islam <sakibbhaiya143@gmail.com>
2023-09-14 23:27:04 +0600

    Cart delete api

--- index.js
+++ index.js
@@ -84,6 +84,14 @@ async function run() {
             }
         })
 
+        app.delete("/remove-cart/:id", verifyJWT ,async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
+            res.send(result)
+
+        })
+
 
 
         // users api
